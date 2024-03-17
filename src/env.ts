dbc02b4dab23fa6f1d68cbcfd781048906ab4db9
VitorLostadaC <vitorlostada@hotmail.com>
2024-03-17 09:17:30 -0300

    feat: update env

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,35 @@
 import { z } from 'zod'
+import { createEnv } from '@t3-oss/env-nextjs'
 
-const envSchema = z.object({
-  OPENAI_API_KEY: z.string(),
-  AWS_S3_REGION: z.string(),
-  AWS_S3_ACCESS_KEY_ID: z.string(),
-  AWS_S3_SECRET_ACCESS_KEY: z.string(),
-  AWS_S3_BUCKET_NAME: z.string()
+export const env = createEnv({
+  /**
+   * Specify your server-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars.
+   */
+  server: {
+    OPENAI_API_KEY: z.string(),
+    AWS_S3_REGION: z.string(),
+    AWS_S3_ACCESS_KEY_ID: z.string(),
+    AWS_S3_SECRET_ACCESS_KEY: z.string(),
+    AWS_S3_BUCKET_NAME: z.string(),
+    DATABASE_URL: z.string()
+  },
+  /**
+   * Specify your client-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars. To expose them to the client, prefix them with
+   * `NEXT_PUBLIC_`.
+   */
+  client: {},
+  /**
+   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
+   * middlewares) or client-side so we need to destruct manually.
+   */
+  runtimeEnv: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    AWS_S3_REGION: process.env.AWS_S3_REGION,
+    AWS_S3_ACCESS_KEY_ID: process.env.AWS_S3_ACCESS_KEY_ID,
+    AWS_S3_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+    DATABASE_URL: process.env.DATABASE_URL
+  }
 })
-
-export const env = envSchema.parse(process.env)
