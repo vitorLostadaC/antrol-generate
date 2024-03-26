79ebd573d5d6e662bf29abecf725887139fc3714
VitorLostadaC <vitorlostada@hotmail.com>
2024-03-25 21:04:17 -0300

    feat: create base of form

--- src/app/[locale]/generate/page.tsx
+++ src/app/[locale]/generate/page.tsx
@@ -1,3 +1,35 @@
+'use client'
+
+import { shapesSchema, stylesSchema } from '@/schemas/icons.schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormProvider, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  prompt: z.string().min(3),
+  color: z.string(),
+  shape: z.enum(shapesSchema.options),
+  styles: z.array(stylesSchema)
+})
+
 export default function Generate() {
-  return <h1>Generate</h1>
+  const methods = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: '',
+      color: '',
+      shape: 'any shape',
+      styles: []
+    }
+  })
+
+  const onSubmit = methods.handleSubmit((data) => {
+    console.log(data)
+  })
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={onSubmit}></form>
+    </FormProvider>
+  )
 }
