df04c6a16e9c82535351ffba7c68f0b82c3ced31
VitorLostadaC <vitorlostada@hotmail.com>
2024-04-14 14:51:37 -0300

    feat: remove query log prisma

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -3,7 +3,7 @@ import { env } from 'process'
 
 const createPrismaClient = () =>
   new PrismaClient({
-    log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+    log: env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error']
   })
 
 const globalForPrisma = globalThis as unknown as {
