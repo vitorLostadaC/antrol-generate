8582bb5f45ea2c4a7e7a1dfe61a9b7a53cf5049a
VitorLostadaC <vitorlostada@hotmail.com>
2024-04-22 21:13:18 -0300

    feat: add cache

--- src/actions/prisma/getGenerations.ts
+++ src/actions/prisma/getGenerations.ts
@@ -1,8 +1,13 @@
 'use server'
 
 import { prisma } from '@/services/prisma'
+import { Prisma } from '@prisma/client'
+import { DefaultArgs } from '@prisma/client/runtime/library'
+import { cache } from 'react'
 
-export const getGenerations = async () => {
-  const generations = await prisma.generation.findMany()
-  return generations
-}
+export const getGenerations = cache(
+  async (options?: Prisma.GenerationFindManyArgs<DefaultArgs>) => {
+    const generations = await prisma.generation.findMany(options)
+    return generations
+  }
+)
