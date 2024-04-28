62242f884713128c366f795e36b6274061c77fec
VitorLostadaC <vitorlostada@hotmail.com>
2024-04-28 14:39:19 -0300

    feat: add translates on pricing paage

--- src/app/[locale]/pricing/page.tsx
+++ src/app/[locale]/pricing/page.tsx
@@ -1,26 +1,27 @@
 import { PriceCards } from './components/PriceCards'
 import { Alert, AlertTitle } from '@/components/ui/alert'
+import { getScopedI18n } from '@/locales/server'
 import { AlertTriangleIcon } from 'lucide-react'
 import Link from 'next/link'
 
-export default function Pricing() {
+export default async function Pricing() {
+  const t = await getScopedI18n('pages.pricing')
+
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <div className="space-y-2 text-center">
-        <h1 className="text-3xl">Buy coins</h1>
-        <p className="text-gray-500">
-          Buy credits to use our services and get access to the best features
-        </p>
+        <h1 className="text-3xl">{t('title')}</h1>
+        <p className="text-gray-500">{t('description')}</p>
       </div>
       <div className="w-full space-y-4">
         <Alert variant="warn" className="w-full">
           <AlertTriangleIcon className="h-4 w-4" />
           <AlertTitle>
-            Please review our{' '}
+            {t('alert.title1')}{' '}
             <Link href={'refund'} className="underline">
-              Refund Policy
+              {t('alert.refund')}
             </Link>{' '}
-            before buying credits. We do not issue refunds at this time.
+            {t('alert.title2')}
           </AlertTitle>
         </Alert>
         <PriceCards />
