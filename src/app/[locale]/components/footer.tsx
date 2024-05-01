d94796ee14a5311fd8edd218319eea10340abe71
VitorLostadaC <vitorlostada@hotmail.com>
2024-05-01 09:02:09 -0300

    feat: base landing page

---
+++ src/app/[locale]/components/footer.tsx
@@ -0,0 +1,115 @@
+import { LogOutIcon } from 'lucide-react'
+
+export const Footer = () => {
+  return (
+    <footer id="footer">
+      <hr className="mx-auto w-11/12" />
+
+      <section className="container grid grid-cols-2 gap-x-12 gap-y-8 py-20 md:grid-cols-4 xl:grid-cols-6">
+        <div className="col-span-full xl:col-span-2">
+          <a href="/" className="flex text-xl font-bold">
+            <LogOutIcon />
+            ShadcnUI/React
+          </a>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-bold">Follow US</h3>
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Github
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Twitter
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Dribbble
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-bold">Platforms</h3>
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Web
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Mobile
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Desktop
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-bold">About</h3>
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Features
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Pricing
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              FAQ
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-bold">Community</h3>
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Youtube
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Discord
+            </a>
+          </div>
+
+          <div>
+            <a href="#" className="opacity-60 hover:opacity-100">
+              Twitch
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section className="container pb-14 text-center">
+        <h3>
+          &copy; 2024 Landing page made by{' '}
+          <a
+            target="_blank"
+            href="https://github.com/leoMirandaa"
+            className="border-primary text-primary transition-all hover:border-b-2"
+          >
+            Leo Miranda
+          </a>
+        </h3>
+      </section>
+    </footer>
+  )
+}
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,210 +1,35 @@
-'use client'
-
-import { Button } from '@/components/ui/button'
-import { signIn } from 'next-auth/react'
+import { About } from './components/about'
+import { Cta } from './components/cta'
+import { FAQ } from './components/faq'
+import { Features } from './components/feature'
+import { Footer } from './components/footer'
+import { Hero } from './components/hero'
+import { HowItWorks } from './components/howItWorks'
+import { Newsletter } from './components/newsletter'
+import { ScrollToTop } from './components/scrollToTop'
+import { Services } from './components/services'
+import { Sponsors } from './components/sponsors'
+import { Team } from './components/team'
+import { Testimonials } from './components/testimonials'
+import Pricing from './pricing/page'
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="mb-4 text-3xl font-bold">Scroll Test</h1>
-      <div className="scrollable rounded-md border border-gray-300 p-4">
-        <div className="fake-content">
-          <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor
-            convallis libero, et pharetra sapien consectetur in. Fusce nec
-            semper ipsum. Nullam varius lorem in lectus tincidunt, vel volutpat
-            ex feugiat. Nam lobortis lectus id urna pulvinar, eget dapibus augue
-            faucibus.
-          </p>
-          <p className="mb-4">
-            Suspendisse vehicula sem id purus blandit commodo. Nunc rhoncus
-            purus id sem vehicula, a consequat tortor consequat. Phasellus
-            euismod mi non eros lobortis blandit. Donec fermentum, mi in
-            accumsan tempor, justo mauris efficitur neque, in volutpat metus
-            urna at libero.
-          </p>
-          <p className="mb-4">
-            Vestibulum ultrices odio id ex rhoncus, vel dapibus lorem varius. Ut
-            lobortis enim non arcu tincidunt, non accumsan tortor convallis.
-            Duis vitae faucibus lacus. Aliquam sed vestibulum justo. Integer
-            mattis magna et arcu volutpat, vel varius dui vehicula.
-          </p>
-          <p className="mb-4">
-            Quisque tempus diam et mi suscipit vehicula. Duis fermentum, arcu
-            nec sollicitudin efficitur, quam justo gravida dui, sed convallis
-            lectus est a tellus. Mauris vitae ex quis ligula efficitur finibus.
-          </p>
-          <p className="mb-4">
-            Fusce sodales risus nec ligula bibendum lobortis. Aliquam erat
-            volutpat. Cras id aliquam ipsum. Aliquam vitae felis condimentum,
-            malesuada ipsum eu, posuere odio. Donec eget sollicitudin tortor.
-          </p>
-          <p className="mb-4">
-            Nullam efficitur lacinia purus, et lacinia nulla. Phasellus interdum
-            libero nec purus rhoncus, eget tincidunt turpis vehicula. Phasellus
-            blandit enim sit amet neque malesuada, eu ultrices nisl aliquet.
-          </p>
-          <p className="mb-4">
-            Sed ultrices, sem et eleifend consectetur, ex ligula tincidunt arcu,
-            vel sollicitudin justo odio sit amet turpis. Duis ullamcorper
-            dapibus lorem, sit amet varius urna commodo vel. Mauris eu quam
-            vehicula, scelerisque lectus vitae, laoreet sapien.
-          </p>
-          <p className="mb-4">
-            Suspendisse potenti. Nam nec eros lectus. Nam ut sem auctor,
-            suscipit orci vel, egestas turpis.
-          </p>
-          <p className="mb-4">
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Ut in tellus sit amet mi consequat luctus.
-            Donec vel odio a tellus consequat ullamcorper. Morbi ullamcorper
-            varius ligula.
-          </p>
-          <p className="mb-4">
-            Vivamus maximus efficitur augue, vitae fringilla libero fermentum
-            nec. Nam ut urna hendrerit, varius turpis ac, pellentesque diam.
-            Proin sagittis quam non eros pretium vestibulum.
-          </p>
-          <p className="mb-4">
-            Phasellus molestie purus nec ipsum efficitur feugiat. Ut in sem nec
-            eros fermentum pretium. Nulla sit amet neque eu dolor sollicitudin
-            venenatis nec in purus.
-          </p>
-        </div>
-      </div>
-      <h1 className="mb-4 text-3xl font-bold">Scroll Test</h1>
-      <Button onClick={() => signIn('google')}>logar</Button>
-      <div className="scrollable rounded-md border border-gray-300 p-4">
-        <div className="fake-content">
-          <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor
-            convallis libero, et pharetra sapien consectetur in. Fusce nec
-            semper ipsum. Nullam varius lorem in lectus tincidunt, vel volutpat
-            ex feugiat. Nam lobortis lectus id urna pulvinar, eget dapibus augue
-            faucibus.
-          </p>
-          <p className="mb-4">
-            Suspendisse vehicula sem id purus blandit commodo. Nunc rhoncus
-            purus id sem vehicula, a consequat tortor consequat. Phasellus
-            euismod mi non eros lobortis blandit. Donec fermentum, mi in
-            accumsan tempor, justo mauris efficitur neque, in volutpat metus
-            urna at libero.
-          </p>
-          <p className="mb-4">
-            Vestibulum ultrices odio id ex rhoncus, vel dapibus lorem varius. Ut
-            lobortis enim non arcu tincidunt, non accumsan tortor convallis.
-            Duis vitae faucibus lacus. Aliquam sed vestibulum justo. Integer
-            mattis magna et arcu volutpat, vel varius dui vehicula.
-          </p>
-          <p className="mb-4">
-            Quisque tempus diam et mi suscipit vehicula. Duis fermentum, arcu
-            nec sollicitudin efficitur, quam justo gravida dui, sed convallis
-            lectus est a tellus. Mauris vitae ex quis ligula efficitur finibus.
-          </p>
-          <p className="mb-4">
-            Fusce sodales risus nec ligula bibendum lobortis. Aliquam erat
-            volutpat. Cras id aliquam ipsum. Aliquam vitae felis condimentum,
-            malesuada ipsum eu, posuere odio. Donec eget sollicitudin tortor.
-          </p>
-          <p className="mb-4">
-            Nullam efficitur lacinia purus, et lacinia nulla. Phasellus interdum
-            libero nec purus rhoncus, eget tincidunt turpis vehicula. Phasellus
-            blandit enim sit amet neque malesuada, eu ultrices nisl aliquet.
-          </p>
-          <p className="mb-4">
-            Sed ultrices, sem et eleifend consectetur, ex ligula tincidunt arcu,
-            vel sollicitudin justo odio sit amet turpis. Duis ullamcorper
-            dapibus lorem, sit amet varius urna commodo vel. Mauris eu quam
-            vehicula, scelerisque lectus vitae, laoreet sapien.
-          </p>
-          <p className="mb-4">
-            Suspendisse potenti. Nam nec eros lectus. Nam ut sem auctor,
-            suscipit orci vel, egestas turpis.
-          </p>
-          <p className="mb-4">
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Ut in tellus sit amet mi consequat luctus.
-            Donec vel odio a tellus consequat ullamcorper. Morbi ullamcorper
-            varius ligula.
-          </p>
-          <p className="mb-4">
-            Vivamus maximus efficitur augue, vitae fringilla libero fermentum
-            nec. Nam ut urna hendrerit, varius turpis ac, pellentesque diam.
-            Proin sagittis quam non eros pretium vestibulum.
-          </p>
-          <p className="mb-4">
-            Phasellus molestie purus nec ipsum efficitur feugiat. Ut in sem nec
-            eros fermentum pretium. Nulla sit amet neque eu dolor sollicitudin
-            venenatis nec in purus.
-          </p>
-        </div>
-      </div>
-      <h1 className="mb-4 text-3xl font-bold">Scroll Test</h1>
-      <div className="scrollable rounded-md border border-gray-300 p-4">
-        <div className="fake-content">
-          <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor
-            convallis libero, et pharetra sapien consectetur in. Fusce nec
-            semper ipsum. Nullam varius lorem in lectus tincidunt, vel volutpat
-            ex feugiat. Nam lobortis lectus id urna pulvinar, eget dapibus augue
-            faucibus.
-          </p>
-          <p className="mb-4">
-            Suspendisse vehicula sem id purus blandit commodo. Nunc rhoncus
-            purus id sem vehicula, a consequat tortor consequat. Phasellus
-            euismod mi non eros lobortis blandit. Donec fermentum, mi in
-            accumsan tempor, justo mauris efficitur neque, in volutpat metus
-            urna at libero.
-          </p>
-          <p className="mb-4">
-            Vestibulum ultrices odio id ex rhoncus, vel dapibus lorem varius. Ut
-            lobortis enim non arcu tincidunt, non accumsan tortor convallis.
-            Duis vitae faucibus lacus. Aliquam sed vestibulum justo. Integer
-            mattis magna et arcu volutpat, vel varius dui vehicula.
-          </p>
-          <p className="mb-4">
-            Quisque tempus diam et mi suscipit vehicula. Duis fermentum, arcu
-            nec sollicitudin efficitur, quam justo gravida dui, sed convallis
-            lectus est a tellus. Mauris vitae ex quis ligula efficitur finibus.
-          </p>
-          <p className="mb-4">
-            Fusce sodales risus nec ligula bibendum lobortis. Aliquam erat
-            volutpat. Cras id aliquam ipsum. Aliquam vitae felis condimentum,
-            malesuada ipsum eu, posuere odio. Donec eget sollicitudin tortor.
-          </p>
-          <p className="mb-4">
-            Nullam efficitur lacinia purus, et lacinia nulla. Phasellus interdum
-            libero nec purus rhoncus, eget tincidunt turpis vehicula. Phasellus
-            blandit enim sit amet neque malesuada, eu ultrices nisl aliquet.
-          </p>
-          <p className="mb-4">
-            Sed ultrices, sem et eleifend consectetur, ex ligula tincidunt arcu,
-            vel sollicitudin justo odio sit amet turpis. Duis ullamcorper
-            dapibus lorem, sit amet varius urna commodo vel. Mauris eu quam
-            vehicula, scelerisque lectus vitae, laoreet sapien.
-          </p>
-          <p className="mb-4">
-            Suspendisse potenti. Nam nec eros lectus. Nam ut sem auctor,
-            suscipit orci vel, egestas turpis.
-          </p>
-          <p className="mb-4">
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Ut in tellus sit amet mi consequat luctus.
-            Donec vel odio a tellus consequat ullamcorper. Morbi ullamcorper
-            varius ligula.
-          </p>
-          <p className="mb-4">
-            Vivamus maximus efficitur augue, vitae fringilla libero fermentum
-            nec. Nam ut urna hendrerit, varius turpis ac, pellentesque diam.
-            Proin sagittis quam non eros pretium vestibulum.
-          </p>
-          <p className="mb-4">
-            Phasellus molestie purus nec ipsum efficitur feugiat. Ut in sem nec
-            eros fermentum pretium. Nulla sit amet neque eu dolor sollicitudin
-            venenatis nec in purus.
-          </p>
-        </div>
-      </div>
-    </div>
+    <>
+      <Hero />
+      <Sponsors />
+      <About />
+      <HowItWorks />
+      <Features />
+      <Services />
+      <Cta />
+      <Testimonials />
+      <Team />
+      <Pricing />
+      <Newsletter />
+      <FAQ />
+      <Footer />
+      <ScrollToTop />
+    </>
   )
 }
