b4e8af43b3317d2785a678f3527e2e39f6023e8a
VitorLostadaC <vitorlostada@hotmail.com>
2024-05-02 21:22:19 -0300

    faq

--- src/app/[locale]/components/faq.tsx
+++ src/app/[locale]/components/faq.tsx
@@ -4,6 +4,7 @@ import {
   AccordionItem,
   AccordionTrigger
 } from '@/components/ui/accordion'
+import { getScopedI18n } from '@/locales/server'
 
 interface FAQProps {
   question: string
@@ -11,40 +12,38 @@ interface FAQProps {
   value: string
 }
 
-const FAQList: FAQProps[] = [
-  {
-    question: 'What is an AI icon generator?',
-    answer:
-      'Our AI icon generator uses advanced algorithms to help you design custom icons effortlessly. Simply input your preferences, and our tool crafts icons tailored to your needs.',
-    value: 'item-1'
-  },
-  {
-    question: 'Is it easy to use for beginners?',
-    answer:
-      'Absolutely! Our platform is designed with simplicity in mind. No prior design experience is needed—just select your preferences, and our AI will handle the rest.',
-    value: 'item-2'
-  },
-  {
-    question: 'Can I use the icons commercially?',
-    answer:
-      'Yes, all icons created with our generator can be used for both personal and commercial projects. You have full rights to use them in your work.',
-    value: 'item-3'
-  },
-  {
-    question: 'How can I get help if I have issues using the tool?',
-    answer:
-      'Our support team is here to help! Contact us via email for any assistance you may need.',
-    value: 'item-4'
-  }
-]
+export const FAQ = async () => {
+  const t = await getScopedI18n('pages.landing-pages.faq')
 
-export const FAQ = () => {
+  const FAQList: FAQProps[] = [
+    {
+      question: t('questions.question1.question'),
+      answer: t('questions.question1.answer'),
+      value: 'item-1'
+    },
+    {
+      question: t('questions.question2.question'),
+      answer: t('questions.question2.answer'),
+      value: 'item-2'
+    },
+    {
+      question: t('questions.question3.question'),
+      answer: t('questions.question3.answer'),
+      value: 'item-3'
+    },
+    {
+      question: t('questions.question4.question'),
+      answer: t('questions.question4.answer'),
+      value: 'item-4'
+    }
+  ]
+
   return (
     <section id="faq" className="py-24 sm:py-32">
       <h2 className="mb-4 text-3xl font-bold md:text-4xl">
-        Frequently Asked{' '}
+        {t('title.pt1')}{' '}
         <span className="bg-gradient-to-b from-primary/60 to-primary bg-clip-text text-transparent">
-          Questions
+          {t('title.pt2')}
         </span>
       </h2>
 
@@ -61,12 +60,12 @@ export const FAQ = () => {
       </Accordion>
 
       <h3 className="mt-4 text-sm font-medium text-muted-foreground">
-        Still have questions?{' '}
+        {t('still-have-questions')}{' '}
         <a
           href="#"
           className="border-primary text-primary transition-all hover:border-b-2"
         >
-          Contact us
+          {t('contact-us')}
         </a>
       </h3>
     </section>
