ea72e96721aa7897b0de1db24b670a424a42f201
VitorLostadaC <vitorlostada@hotmail.com>
2024-05-14 21:52:09 -0300

    fix: little fix

--- src/locales/server.ts
+++ src/locales/server.ts
@@ -5,7 +5,13 @@ import { createI18nServer } from 'next-international/server'
 export const { getI18n, getScopedI18n, getStaticParams, getCurrentLocale } =
   createI18nServer({
     en: () => import('./dictionaries/en.json'),
-    pt: () => import('./dictionaries/pt.json')
+    pt: () => import('./dictionaries/pt.json'),
+    zh: () => import('./dictionaries/zh.json'),
+    ru: () => import('./dictionaries/ru.json'),
+    ja: () => import('./dictionaries/ja.json'),
+    fr: () => import('./dictionaries/fr.json'),
+    es: () => import('./dictionaries/es.json'),
+    ar: () => import('./dictionaries/ar.json')
   } as const satisfies Record<
     Locale,
     () => Promise<{ default: AbstractDictionarie }>
