07d071fb36d4395f44c61bd2d8ae4f517ba80800
VitorLostadaC <vitorlostada@hotmail.com>
2024-05-14 21:45:49 -0300

    feat: add more languages

--- src/data/locales.ts
+++ src/data/locales.ts
@@ -1,2 +1,2 @@
-export const locales = ['en', 'pt'] as const
+export const locales = ['en', 'pt', 'zh', 'ru', 'ja', 'fr', 'es', 'ar'] as const
 export type Locale = (typeof locales)[number]
--- src/locales/client.ts
+++ src/locales/client.ts
@@ -6,7 +6,13 @@ import { createI18nClient } from 'next-international/client'
 export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale } =
   createI18nClient({
     en: () => import('./dictionaries/en.json'),
-    pt: () => import('./dictionaries/pt.json')
+    pt: () => import('./dictionaries/pt.json'),
+    zh: () => import('./dictionaries/zh.json'),
+    ru: () => import('./dictionaries/ru.json'),
+    ja: () => import('./dictionaries/ja.json'),
+    fr: () => import('./dictionaries/fr.json'),
+    es: () => import('./dictionaries/es.json'),
+    ar: () => import('./dictionaries/ar.json')
   } as const satisfies Record<
     Locale,
     () => Promise<{ default: AbstractDictionarie }>
