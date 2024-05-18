c8fa51f28775dc0c6cd28b010f392e725d90a9d7
VitorLostadaC <vitorlostada@hotmail.com>
2024-05-18 18:19:02 -0300

    feat: back gallery

--- src/app/[locale]/gallery/page.tsx
+++ src/app/[locale]/gallery/page.tsx
@@ -20,18 +20,13 @@ export default async function Gallery({
   params: { locale: string }
 }) {
   const t = await getScopedI18n('pages.gallery')
-  const response = await getGenerations({
+  const generations: Generation[] = await getGenerations({
     take: 50
   })
-
-  const generations: Generation[] = response.generations
-
-  const date = response.date
 
   return (
     <div className="mx-auto w-full max-w-3xl space-y-6">
       <h2 className="text-2xl">
-        {new Date(date).toLocaleString()}
         <span className="text-sm text-foreground/80">({t('title.pt2')})</span>
       </h2>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 md:grid-cols-5 "></div>{' '}
