0dfde815fb91f1f2429bea8a5997aaef9307bbe4
VitorLostadaC <vitorlostada@hotmail.com>
2024-05-18 18:41:29 -0300

    feat: responsible confirm page

--- src/app/[locale]/generate/steps/confirm/confirmStep.tsx
+++ src/app/[locale]/generate/steps/confirm/confirmStep.tsx
@@ -7,6 +7,7 @@ import { useScopedI18n } from '@/locales/client'
 import { StepTitle } from '../../components/stepTitle'
 import posthog from 'posthog-js'
 import { Fragment, useEffect } from 'react'
+import { color } from 'framer-motion'
 
 export const ConfirmStep = () => {
   const { getValues } = useFormContext<FormSchema>()
@@ -35,13 +36,45 @@ export const ConfirmStep = () => {
     }
   ]
 
-  return (
-    <div className="flex flex-col gap-2">
-      <StepTitle title={t('title')} description={t('description')} />
+  const mobileShapeAndColors = (
+    <div className="block w-full space-y-2 sm:hidden">
+      <h2 className="font-semibold">{t('steps.color')}</h2>
+      <div className="grid grid-cols-2 gap-6">
+        {colors.map((color) => {
+          if (!color.color) return <Fragment key={color.name} />
 
-      <h2 className="font-semibold">{t('steps.prompt')}</h2>
-      <p>{getValues('prompt')}</p>
+          return (
+            <div
+              className="flex flex-col justify-center gap-2 text-center"
+              key={color.name}
+            >
+              <div
+                className="aspect-square w-full rounded-md"
+                style={{ backgroundColor: color.color }}
+              />
+              <h2 className="text-nowrap text-foreground/80">{color.name}</h2>
+            </div>
+          )
+        })}
+      </div>
+      <h2 className="font-semibold">{t('steps.shape')}</h2>
 
+      <div className="grid grid-cols-2 gap-6">
+        <div className="w-full space-y-2 text-center">
+          <img
+            src={currentShape?.image.src}
+            alt={currentShape?.name}
+            className={cn('aspect-square rounded-md bg-contain')}
+          />
+
+          <p className="text-foreground/80">{currentShape?.name}</p>
+        </div>
+      </div>
+    </div>
+  )
+
+  const desktopShapeAndColors = (
+    <div className="hidden w-full space-y-2 sm:block">
       <div className="grid grid-cols-4 gap-6">
         <h2 className="font-semibold">{t('steps.shape')}</h2>
         <h2 className="font-semibold">{t('steps.color')}</h2>
@@ -74,9 +107,21 @@ export const ConfirmStep = () => {
           )
         })}
       </div>
+    </div>
+  )
+
+  return (
+    <div className="flex flex-col gap-2">
+      <StepTitle title={t('title')} description={t('description')} />
+
+      <h2 className="font-semibold">{t('steps.prompt')}</h2>
+      <p>{getValues('prompt')}</p>
+
+      {mobileShapeAndColors}
+      {desktopShapeAndColors}
 
       <h2 className="font-semibold">{t('steps.style')}</h2>
-      <div className="grid grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 gap-6 sm:grid-cols-4">
         {getValues('styles').map((style) => {
           const predefinedStyle = predefinedStyles.find(
             (s) => s.style === style
